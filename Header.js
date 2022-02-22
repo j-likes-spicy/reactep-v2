227b91f0211bb1cfa16083afafd98551a0928e5b
j-likes-spicy <77315222+j-likes-spicy@users.noreply.github.com>
2022-02-21 23:27:10 -0700

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <img src="../img/react-small-img.png" className="nav--icon" alt="" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </header>
+  );
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+/_
+This is a very simple, yet clean UI project that was created using CSS and React.  
+_/
+# reactep-v2
